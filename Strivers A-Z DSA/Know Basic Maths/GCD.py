d6d35b6a4c976859c0552440f8bbce2ccfe02172
praghu-b <prakashbalan555@gmail.com>
2025-08-23 00:20:42 +0530

    optimize: gcd from strivers

--- Strivers A-Z DSA/Know Basic Maths/GCD.py
+++ Strivers A-Z DSA/Know Basic Maths/GCD.py
@@ -1,19 +1,10 @@
 def gcd(num1, num2):
-    num1_divs = set()
-    num2_divs = set()
-    gcd = 0
     
-    for i in range(1, min(num1, num2)+1):
-        if num1%i == 0:
-            num1_divs.add(i)
-            if i in num2_divs and i > gcd:
-                gcd = i
-        if num2%i == 0:
-            num2_divs.add(i)
-            if i in num1_divs and i > gcd:
-                gcd = i
-
-    return gcd
+    for i in range(min(num1, num2), 0, -1):
+        if num1%2 == 0 and num2%2 == 0:
+            return i
+    
+    return 1
     
 
 num1 = int(input('Num1: '))
