e18e9e1a03ae1985bfd264e9d741425e8da35cca
praghu-b <prakashbalan555@gmail.com>
2025-08-22 20:25:59 +0530

    pattern: increasing number triangle

---
+++ Strivers A-Z DSA/Patterns/increase_triangle.py
@@ -0,0 +1,11 @@
+def increasing_triangle(num):
+    sum=1
+
+    for i in range(num):
+        for j in range(i+1):
+            print(sum, end=' ')
+            sum+=1
+        print()
+    
+
+increasing_triangle(int(input('Number? ')))
