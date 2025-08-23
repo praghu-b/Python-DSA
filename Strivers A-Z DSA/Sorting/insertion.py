218313f5864d6e6f32cc72d752c66fda02687f7b
praghu-b <prakashbalan555@gmail.com>
2025-08-24 00:59:52 +0530

    strivers - insertion > sorting

---
+++ Strivers A-Z DSA/Sorting/insertion.py
@@ -0,0 +1,14 @@
+def insertion(nums):
+    for i in range(1, len(nums)):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > key:
+            nums[j+1] = nums[j]
+            j-=1
+        nums[j+1] = key
+    
+    return nums
+
+
+nums = [13,46,24,52,20,9]
+print(insertion(nums))
