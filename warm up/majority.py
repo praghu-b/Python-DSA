d93d0a402ca7b0c609c780ee286272c6c4df16b8
praghu-b <prakashbalan555@gmail.com>
2025-12-11 21:31:30 +0530

    update: solve find majority element with optimal solution

--- warm up/majority.py
+++ warm up/majority.py
@@ -1,23 +1,38 @@
+# def find_mjr_element(nums):
+#     count = {}
+#     max_count = 0
+#     mjr = 0
+
+#     for num in nums:
+#         if num not in count:
+#             count[num] = 1
+#         else:
+#             count[num] += 1
+
+#     for item in count:
+#         if count[item] > max_count:
+#             max_count = count[item]
+#             mjr = item
+
+#     if count[mjr] > len(nums)//2: return mjr
+
+#     return 0
+
 def find_mjr_element(nums):
-    count = {}
-    max_count = 0
-    mjr = 0
+    candidate = None
+    count = 0
 
     for num in nums:
-        if num not in count:
-            count[num] = 1
+        if count == 0:
+            candidate = num
+            count += 1
+        elif num == candidate:
+            count += 1
         else:
-            count[num] += 1
+            count -= 1
 
-    for item in count:
-        if count[item] > max_count:
-            max_count = count[item]
-            mjr = item
-
-    if count[mjr] > len(nums)//2: return mjr
-
-    return 0
+    return candidate
     
 
-nums = [2,1,1,1,1,3,3]
+nums = [2,1,1,1,1,3,3,3,3,3,3]
 print(find_mjr_element(nums))
