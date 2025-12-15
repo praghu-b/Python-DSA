719785cd910243886a84e50e02a4c4a97d1d3659
praghu-b <prakashbalan555@gmail.com>
2025-12-15 23:31:21 +0530

    update: solve three sum with optimal solution by sorting and using two pointer

--- warm up/three_sum.py
+++ warm up/three_sum.py
@@ -11,7 +11,42 @@ def three_sum_bf(nums):
                         res.append(sub_arr)
                     
     return res
+
+def three_sum_opt(nums):
+    nums.sort()
+    res = []
+    n = len(nums)
     
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        
+        if nums[i] > 0:
+            break
+        
+        j = i + 1
+        k = n - 1
+        
+        while j < k:
+            if nums[j] + nums[k] + nums[i] == 0:
+                res.append([nums[i], nums[j], nums[k]])
+                    
+                j += 1
+                k -= 1
+                
+                while j < k and nums[j] == nums[j - 1]:
+                    j += 1
+                
+                while j < k and nums[k] == nums[k + 1]:
+                    k -= 1
+                
+            elif nums[j] + nums[k] + nums[i] < 0:
+                j += 1
+            else:
+                k -= 1
+        
+    return res
+
 
 nums = [-100000,50000,50000,0]
-print(three_sum(nums))
+print(three_sum_opt(nums))
