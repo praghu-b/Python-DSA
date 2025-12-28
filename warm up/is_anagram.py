e4a134f0e12168212634aa1a6aaeee21d08fe2d3
praghu-b <prakashbalan555@gmail.com>
2025-12-28 18:50:03 +0530

    update: solve anagram problem with optimal solution

--- warm up/is_anagram.py
+++ warm up/is_anagram.py
@@ -1,3 +1,4 @@
+# brute force
 def is_anagram(str1, str2):
     if len(str1) != len(str2):
         return False
@@ -12,6 +13,24 @@ def is_anagram(str1, str2):
     return freq1 == freq2
 
 
+# optimal
+def is_anagram_opt(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    
+    freq = {}
+    
+    for i in range(len(str1)):
+        freq[str1[i]] = freq.get(str1[i], 0) + 1
+        freq[str2[i]] = freq.get(str2[i], 0) - 1
+        
+    for item in freq:
+        if freq[item] != 0:
+            return False
+        
+    return True
+
+
 str1 = 'anagram'
 str2 = 'nagaram'
-print(is_anagram("abcdefghijklmnopqrstuvwxyz", "zyxwvutsrqponmlkjihgfedcba"))
+print(is_anagram_opt("abcdefghijklmnopqrstvwxyz", "zyxwvutsrqponmlkjihgfedcba"))
